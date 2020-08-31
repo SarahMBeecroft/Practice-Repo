480d04d9e2e6e7496c2ef4f18243072267f9cd19
Sarah B <smblair87@gmail.com>
2020-08-31 14:57:52 -0700

    More practice coding

--- practice.py
+++ practice.py
@@ -1,37 +1,27 @@
-def manage_students():
-  print("What would you like to do?\n\n1. Add a Student\n2. Delete a Student\n3. Search a Student\n4. Exit/Quit\n")
-  selection = int(input("Enter the corresponding number: "))
+import random 
 
-  while True:
-    if selection == 1:
-      print("Student added successfully")
-      selection_2 = input("Would you like to continue? Yes/No\n")
-      if selection_2 == "yes" or selection_2 == "YES" or selection_2 == "Yes" or selection_2 == "Y" or selection_2 == "y":
-        manage_students()
-      else:
-        break
-      break
-    elif selection == 2:
-      print("Student deleted successfully")
-      selection_2 = input("Would you like to continue? Yes/No\n")
-      if selection_2 == "yes" or selection_2 == "YES" or selection_2 == "Yes" or selection_2 == "Y" or selection_2 == "y":
-        manage_students()
-      else:
-        break
-      break
-    elif selection == 3:
-      print("You have chosen to search for a student")
-      selection_2 = input("Would you like to continue? Yes/No\n")
-      if selection_2 == "yes" or selection_2 == "YES" or selection_2 == "Yes" or selection_2 == "Y" or selection_2 == "y":
-        manage_students()
-      else:
-        break
-      break
-    elif selection == 4:
-      print("You have chosen to quit/exit")
-      break
+def number_guess():
+  print('-' * 24)
+  print("Guess The Number")
+  print('-' * 24)
+  
+  random_number = random.randint(1,10)
+  total_tries = 0
+  guess = 0
+
+  while guess != random_number and total_tries < 5:
+    guess = int(input("Enter a number between 1 and 10 "))
+    if guess < random_number:
+      print("Too low")
+      total_tries += 1
     else: 
-      print("Please enter a valid number.")
-      manage_students()
+      print("Too high")
+      total_tries += 1
+  
+  if guess == random_number:
+    print("You won! It took you ",total_tries, "tries to guess the correct number")
+  else:
+    print("You lost, the correct number was ", random_number)
+  
  
-manage_students()
+number_guess()
