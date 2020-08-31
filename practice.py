dc224603960bc77902a2d8f200f5ccad1137adcb
Sarah B <smblair87@gmail.com>
2020-08-31 14:14:28 -0700

    Coding study progress

--- practice.py
+++ practice.py
@@ -5,14 +5,27 @@ def manage_students():
   while True:
     if selection == 1:
       print("Student added successfully")
-      selection_2 = input("Would you like to continue? Yes/No")
-        if selection == "yes" or selection == "YES" or selection == "Yes" or selection == "Y" or selection == "y"
+      selection_2 = input("Would you like to continue? Yes/No\n")
+      if selection_2 == "yes" or selection_2 == "YES" or selection_2 == "Yes" or selection_2 == "Y" or selection_2 == "y":
+        manage_students()
+      else:
+        break
       break
     elif selection == 2:
       print("Student deleted successfully")
+      selection_2 = input("Would you like to continue? Yes/No\n")
+      if selection_2 == "yes" or selection_2 == "YES" or selection_2 == "Yes" or selection_2 == "Y" or selection_2 == "y":
+        manage_students()
+      else:
+        break
       break
     elif selection == 3:
       print("You have chosen to search for a student")
+      selection_2 = input("Would you like to continue? Yes/No\n")
+      if selection_2 == "yes" or selection_2 == "YES" or selection_2 == "Yes" or selection_2 == "Y" or selection_2 == "y":
+        manage_students()
+      else:
+        break
       break
     elif selection == 4:
       print("You have chosen to quit/exit")
