188aff21b5f43aa0ba2b9c45d1d98ec36c81fa3b
Sarah Beecroft <44879145+SarahMBeecroft@users.noreply.github.com>
2020-10-04 10:57:24 -0700

    Python practice

--- practice.py
+++ practice.py
@@ -1,27 +1,7 @@
-import random 
+def nested_loops():
+    for outer in range(1, 6):
+        for inner in range(1, 6):
+            print(outer, inner)
 
-def number_guess():
-  print('-' * 24)
-  print("Guess The Number")
-  print('-' * 24)
-  
-  random_number = random.randint(1,10)
-  total_tries = 0
-  guess = 0
 
-  while guess != random_number and total_tries < 5:
-    guess = int(input("Enter a number between 1 and 10 "))
-    if guess < random_number:
-      print("Too low")
-      total_tries += 1
-    else: 
-      print("Too high")
-      total_tries += 1
-  
-  if guess == random_number:
-    print("You won! It took you ",total_tries, "tries to guess the correct number")
-  else:
-    print("You lost, the correct number was ", random_number)
-  
- 
-number_guess()
+nested_loops()
