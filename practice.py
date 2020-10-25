61be5c811e6d13fa3094dbcfef37e25c1eb13710
Sarah Beecroft <44879145+SarahMBeecroft@users.noreply.github.com>
2020-10-24 17:21:52 -0700

    Python practice

--- practice.py
+++ practice.py
@@ -1,6 +1,18 @@
-def f_to_c(f_temp):
-    f_temp = (f_temp - 32) * 5/9
-    print(f_temp)
+name = "Sarah"
+first_letter = name[0]
+second_letter = name[1]
+third_letter = name[2]
+fourth_letter = name[3]
+fifth_letter = name[4]
+print(first_letter + second_letter + third_letter + fourth_letter + fifth_letter)
 
+age = 33
 
-f100_in_celsius = f_to_c(100)
+if age >= 65:
+    print("You are a senior")
+elif age < 20 and age >= 13:
+    print("You are a teenager")
+elif age < 13:
+    print("You are a child")
+else:
+    print("You are an adult")
